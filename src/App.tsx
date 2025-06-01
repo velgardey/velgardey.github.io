07327047f898f9905823e721b3724f6bb95000d5
Mriganka Dey <arch.velgardey@gmail.com>
2025-06-01 14:40:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -260,7 +260,7 @@ const App: React.FC = () => {
         } else if (planet.label === 'Back') {
             setNextPlanets([
                 createRandomPlanet('Projects', '#3A86FF'),
-                createRandomPlanet('Resume', '#8338EC', 'https://drive.google.com/file/d/1ibIMOpSetu-Vz7JqzveKY-YYypjrqwWU/view?usp=sharing'),
+                createRandomPlanet('Resume', '#8338EC', 'https://drive.google.com/file/d/1CXzOWOduSh0ht00PQGUxPpgxpdD6In0n/view?usp=sharing'),
                 createRandomPlanet('Contact', '#FF006E'),
             ]);
             setCurrentPage('main');
